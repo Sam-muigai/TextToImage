0ddc500ef5d6b7829202972dcce8c751b5f1a9ac
sammu <muigai.samuel21@students.dkut.ac.ke>
2023-03-18 10:57:21 +0300

    Commit

--- app/src/androidTest/java/com/sam/texttoimage/HomeScreenTest.kt
+++ app/src/androidTest/java/com/sam/texttoimage/HomeScreenTest.kt
@@ -1,9 +1,11 @@
 package com.sam.texttoimage
 
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.sam.texttoimage.feature_home.presentation.home.HomeScreen
 import com.sam.texttoimage.feature_home.presentation.home.HomeScreenState
+import com.sam.texttoimage.ui.theme.TextToImageTheme
 import org.junit.Rule
 import org.junit.Test
 
@@ -11,18 +13,23 @@ class HomeScreenTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+
+    private val lightTheme = AppCompatDelegate.MODE_NIGHT_NO
+
     @Test
     fun search_field_shows_initially() {
         composeTestRule.setContent {
-            HomeScreen(
-                homeScreenState = HomeScreenState.Empty,
-                onImageClicked = {},
-                value = "",
-                onClearClicked = { },
-                onSearch = { },
-                onSettingsClicked = { },
-                onValueChange = {}
-            )
+            TextToImageTheme(theme = lightTheme) {
+                HomeScreen(
+                    homeScreenState = HomeScreenState.Empty,
+                    onImageClicked = {},
+                    value = "",
+                    onClearClicked = { },
+                    onSearch = { },
+                    onSettingsClicked = { },
+                    onValueChange = {}
+                )
+            }
         }
         composeTestRule
             .onNodeWithText("Type a prompt")
@@ -30,17 +37,20 @@ class HomeScreenTest {
     }
 
     @Test
-    fun error_animation_shows_when_error_occurs(){
+    fun error_animation_shows_when_error_occurs() {
         composeTestRule.setContent {
-            HomeScreen(
-                homeScreenState = HomeScreenState.Error("Error occurred."),
-                onImageClicked = {},
-                value = "",
-                onClearClicked = { /*TODO*/ },
-                onSearch = { /*TODO*/ },
-                onSettingsClicked = { /*TODO*/ },
-                onValueChange = {}
-            )
+
+            TextToImageTheme(theme = lightTheme) {
+                HomeScreen(
+                    homeScreenState = HomeScreenState.Error("Error occurred."),
+                    onImageClicked = {},
+                    value = "",
+                    onClearClicked = { /*TODO*/ },
+                    onSearch = { /*TODO*/ },
+                    onSettingsClicked = { /*TODO*/ },
+                    onValueChange = {}
+                )
+            }
         }
         composeTestRule
             .onNodeWithContentDescription("Error Animation")
@@ -48,17 +58,19 @@ class HomeScreenTest {
     }
 
     @Test
-    fun loading_animation_shows_when_loading(){
+    fun loading_animation_shows_when_loading() {
         composeTestRule.setContent {
-            HomeScreen(
-                homeScreenState = HomeScreenState.Loading,
-                onImageClicked = {},
-                value = "",
-                onClearClicked = { /*TODO*/ },
-                onSearch = { /*TODO*/ },
-                onSettingsClicked = { /*TODO*/ },
-                onValueChange = {}
-            )
+            TextToImageTheme(theme = lightTheme) {
+                HomeScreen(
+                    homeScreenState = HomeScreenState.Loading,
+                    onImageClicked = {},
+                    value = "",
+                    onClearClicked = { /*TODO*/ },
+                    onSearch = { /*TODO*/ },
+                    onSettingsClicked = { /*TODO*/ },
+                    onValueChange = {}
+                )
+            }
         }
         composeTestRule
             .onNodeWithContentDescription("Loading")
@@ -66,17 +78,39 @@ class HomeScreenTest {
     }
 
     @Test
-    fun when_search_is_clicked_it_gain_focus(){
+    fun search_field_is_not_focused_initially() {
         composeTestRule.setContent {
-            HomeScreen(
-                homeScreenState = HomeScreenState.Empty,
-                onImageClicked = {},
-                value = "",
-                onClearClicked = { /*TODO*/ },
-                onSearch = { /*TODO*/ },
-                onSettingsClicked = { /*TODO*/ },
-                onValueChange = {}
-            )
+            TextToImageTheme(theme = lightTheme) {
+                HomeScreen(
+                    homeScreenState = HomeScreenState.Empty,
+                    onImageClicked = {},
+                    value = "",
+                    onClearClicked = { /*TODO*/ },
+                    onSearch = { /*TODO*/ },
+                    onSettingsClicked = { /*TODO*/ },
+                    onValueChange = {}
+                )
+            }
+        }
+        composeTestRule
+            .onNodeWithText("Type a prompt")
+            .assertIsNotFocused()
+    }
+
+    @Test
+    fun when_search_is_clicked_it_gain_focus() {
+        composeTestRule.setContent {
+            TextToImageTheme(theme = lightTheme) {
+                HomeScreen(
+                    homeScreenState = HomeScreenState.Empty,
+                    onImageClicked = {},
+                    value = "",
+                    onClearClicked = { /*TODO*/ },
+                    onSearch = { /*TODO*/ },
+                    onSettingsClicked = { /*TODO*/ },
+                    onValueChange = {}
+                )
+            }
         }
         composeTestRule
             .onNodeWithText("Type a prompt")
@@ -84,6 +118,23 @@ class HomeScreenTest {
             .assertIsFocused()
     }
 
-
-
+    @Test
+    fun when_search_field_has_text_clear_icon_is_visible() {
+        composeTestRule.setContent {
+            TextToImageTheme(theme = lightTheme) {
+                HomeScreen(
+                    homeScreenState = HomeScreenState.Empty,
+                    onImageClicked = {},
+                    value = "Cow eating grass",
+                    onClearClicked = { /*TODO*/ },
+                    onSearch = { /*TODO*/ },
+                    onSettingsClicked = { /*TODO*/ },
+                    onValueChange = {}
+                )
+            }
+        }
+        composeTestRule
+            .onNodeWithContentDescription("Clear text")
+            .assertIsDisplayed()
+    }
 }
